164e9a144209e59093b74d4c3531200c91a827ec
Maru Newby <marun@redhat.com>
2015-07-01 14:27:27 -0700

    Configure dnf in update script
    Add some local configuration to make package installation faster and
    more reliable.

--- script/update.sh
+++ script/update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -eux
+
+echo 'deltarpm=0' >> /etc/dnf/dnf.conf
+sed -i -e 's+#baseurl=+baseurl=+' /etc/yum.repos.d/*.repo
+sed -i -e 's+metalink=+#metalink=+' /etc/yum.repos.d/*.repo
+sed -i -e 's+download.fedoraproject.org/pub+mirror.hmc.edu+' /etc/yum.repos.d/*.repo
+
 if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     echo "==> Applying updates"
     yum -y update
